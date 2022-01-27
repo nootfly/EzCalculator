db827f657e267a59f6cb17d2c063b9bd51310b37
noot <nootfy@gmail.com>
2022-01-27 17:16:42 +1100

    Initial Commit

---
+++ Shared/EzCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  EzCalculatorApp.swift
+//  Shared
+//
+//  Created by Noot Fang on 27/1/22.
+//
+
+import SwiftUI
+
+@main
+struct EzCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
